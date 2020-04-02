2c2f8f971cf734eb917918299ac4d7e1f5c9c83b
cyrilvincent <contact@cyrilvincent.com>
2020-04-02 14:21:05 +0200

    CEA 1.25/3

--- tp2.py
+++ tp2.py
@@ -2,3 +2,11 @@
 # Filtrer les nombres premiers
 # Sur le résultat précédent filtrer les nombres pairs
 # Tester avec un enorme range(100000000000000000)
+
+import tp1
+l = range(100000000000000000000000000000)
+res = filter(tp1.isPrime, l)
+res = filter(lambda x : x % 2 == 0, res)
+res = map(lambda x : x ** 0.5, res)
+for i in res:
+    print(i)
