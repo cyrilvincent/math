edea04ecc4922a2251c5cb0ae3c5e2ea4a45a992
cyrilvincent <contact@cyrilvincent.com>
2020-04-02 11:40:34 +0200

    CEA 0.5/3

---
+++ monmodule.py
@@ -0,0 +1,7 @@
+toto = 3
+
+def titi():
+    return 0
+
+if __name__ == '__main__':
+    print(titi())
---
+++ tp1.py
@@ -0,0 +1,6 @@
+# Créer la fonction isEven(x:int)
+# Tester la faction dans un if __name__ ...
+# Créer la fonction isPrime(x:int)
+# Un nombre est premier > 1 et divisible uniquement par 1  et lui-même
+# Ex : 7 est premier, 8 est divisble par 2 et 4 n'est pas premier
+# Réciproque : Tout nombre > 1 est premier sauf s'il a un diviseur entre 2 et n-1
