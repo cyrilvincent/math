a0432babb97db44fe16a75fd4a66b4feb39a616a
cyrilvincent <contact@cyrilvincent.com>
2020-04-16 14:06:39 +0200

    CEA 2.75/3

---
+++ demoknnheart.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import sklearn.linear_model as sklm
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn.neighbors as nn
+
+data = pd.read_csv("data/heartdisease/data_cleaned_up.csv")
+y = data['num']
+x = data.drop("num", 1)
+
+model = nn.KNeighborsClassifier(n_neighbors=3)
+model.fit(x, y)
+print(model.score(x,y))
+print(model.predict(x))
