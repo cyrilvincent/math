d625e5feaec2aa4c0d2732f6e80a5008711424a5
cyrilvincent <contact@cyrilvincent.com>
2020-04-16 14:09:06 +0200

    CEA 2.75/3

---
+++ tpcancer.py
@@ -0,0 +1,7 @@
+# Charger avec pandas data/breast-cancer/data.csv
+# Effacer la colonne id
+# y = colonne diagnosis
+# x = autres colonnes (30)
+# Calculer un knn sur le dataset avec k = 3, 5, 7, ...
+# Afficher score
+# Afficher les prédictions
