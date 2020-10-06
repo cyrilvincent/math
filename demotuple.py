17a9f6890767d1f80380557deb5f16cfa8549116
cyrilvincent <contact@cyrilvincent.com>
2020-10-06 13:07:35 +0200

    CEA

--- demotuple.py
+++
@@ -1,8 +0,0 @@
-def toto():
-    return 1,2,3,4
-
-if __name__ == '__main__':
-    x,y,z,a = toto()
-    print(x,y,z,a)
-    res = toto()
-    print(res)
--- monmodule.py
+++
@@ -1,7 +0,0 @@
-toto = 3
-
-def titi():
-    return 0
-
-if __name__ == '__main__':
-    print(titi())
