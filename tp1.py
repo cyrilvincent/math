34f76078674b8aa4706a8d22c0522e96ed7b94ef
cyrilvincent <contact@cyrilvincent.com>
2020-10-12 11:01:20 +0200

    CEA

--- tp1.py
+++ tp1.py
@@ -1,3 +1,5 @@
+import math
+
 # Afficher les nombres pairs < 100
 # Créer la variable year = 2020
 # Saisir votre année de naissance
@@ -5,4 +7,27 @@
 # Saisir un entier et afficher s'il est pair
 # Saisir un entier et afficher s'il est premier
 
-print()
+# for i in range(0,100,2):
+#     print(i)
+# year = 2020
+# birth = int(input("Année de naissance:"))
+# age = year - birth
+# print(f"Vous avez {age} an(s)")
+# if birth % 2 == 0:
+#     print("Pair")
+# else:
+#     print("Impair")
+nb = int(input("Saisir un nombre:"))
+if nb < 2:
+    print("Non premier")
+else:
+    is_prime = True
+    for div in range(2,int(math.sqrt(nb)) + 1):
+        if nb % div == 0:
+            print("Non premier")
+            is_prime = False
+            break
+    if is_prime:
+        print("Premier")
+
+
