efb95e79b6b69710eb261549bd73587d82b45467
cyrilvincent <contact@cyrilvincent.com>
2020-10-13 11:00:55 +0200

    CEA

--- demomesure.py
+++ demomesure.py
@@ -16,3 +16,27 @@ plt.show()
 # bruit_abs = |col2 - col4|
 # bruit_square = (col2 - col4)²
 # Trouver les index temps où |col2 - col4| > delta
+
+delta = 1
+vt = mesures[:,2]
+vm = mesures[:,4]
+ix = mesures[:,0]
+print(np.min(vm), np.max(vm))
+
+def get_mesures_errors(ix,vt,vm, filtre):
+    """
+    Calcul les erreurs entre VT et VM
+    :param ix: index de temps
+    :param vt: volt théoriques
+    :param vm: volt mesurés
+    :param filtre: filtre de bruit
+    :return: liste de tuples (ix, vt, vm)
+    """
+    triplets = list(zip(ix[filtre], vt[filtre], vm[filtre]))
+    return triplets
+
+bruit_abs = np.abs(vm - vt)
+bruit_square = (vm - vt) ** 2
+filtre = bruit_abs > delta
+res = get_mesures_errors(ix,vm,vt,filtre)
+print(res)
