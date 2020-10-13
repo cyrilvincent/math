768493f19849d90fc3fb186978f5bc8403cf9f12
cyrilvincent <contact@cyrilvincent.com>
2020-10-13 08:57:16 +0200

    CEA

--- democovid.py
+++ democovid.py
@@ -1,5 +1,6 @@
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
 with open("data/covid/covid-france.txt") as f:
     reader = csv.DictReader(f)
@@ -11,7 +12,18 @@ with open("data/covid/covid-france.txt") as f:
         nbcas.append(float(row["NbCas"]))
         dc.append(float(row["DC"]))
 
-plt.yscale("log")
-plt.plot(ix, nbcas)
-plt.plot(ix, dc)
+ix = np.array(ix)
+nbcas = np.array(nbcas)
+dc = np.array(dc)
+filtre = nbcas > 10
+nbcas_filtre = nbcas[filtre]
+dc_filtre = dc[filtre]
+ix_filtre = ix[filtre]
+letalite = dc_filtre / nbcas_filtre
+
+#plt.yscale("log")
+plt.plot(nbcas)
+plt.plot(dc)
+plt.show()
+plt.plot(letalite)
 plt.show()
