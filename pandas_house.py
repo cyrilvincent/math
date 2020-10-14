d6fb5006ada56649c45adc85f8e1a0f03d910eae
cyrilvincent <contact@cyrilvincent.com>
2020-10-14 09:50:23 +0200

    CEA

--- pandas_house.py
+++ pandas_house.py
@@ -2,7 +2,7 @@ import pandas
 import matplotlib.pyplot as plt
 import sqlite3
 
-#dataframe = pandas.read_csv("data/house/house.csv")
+dataframe = pandas.read_csv("data/house/house.csv",na_values=".")
 # with sqlite3.connect("data/house/house.db3") as conn:
 #     dataframe = pandas.read_sql("select loyer, surface from house", conn)
 dataframe = pandas.read_excel("data/house/house.xlsx")
