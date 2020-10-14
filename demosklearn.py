bcf16da0c9668ba6b0ca3ff88ecb7c80e1b2559d
cyrilvincent <contact@cyrilvincent.com>
2020-10-14 13:17:08 +0200

    CEA

--- demosklearn.py
+++ demosklearn.py
@@ -1,15 +1,22 @@
 import numpy as np
 import pandas
 import sklearn.linear_model as lm
+import sklearn.preprocessing as pp
+import sklearn.pipeline as pipe
 import matplotlib.pyplot as plt
+import sklearn.model_selection as ms
 
 data = pandas.read_csv("data/house/house.csv")
 x = data.surface.values.reshape(-1,1)
 y = data.loyer
 
-model = lm.LinearRegression()
-model.fit(x, y)
-print(model.intercept_, model.coef_)
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x,y,train_size=0.8, test_size=0.2)
+
+#model = lm.LinearRegression()
+model = pipe.make_pipeline(pp.PolynomialFeatures(3), lm.Ridge())
+model.fit(xtrain, ytrain)
+
+print(model.score(xtest, ytest))
 
 plt.scatter(data.surface, data.loyer)
 plt.plot(np.arange(400), model.predict(np.arange(400).reshape(-1,1)))
