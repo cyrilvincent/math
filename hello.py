244e12fe80eced057d0150c4f65f12b9d3e97080
cyrilvincent <contact@cyrilvincent.com>
2021-03-22 10:34:10 +0100

    CEA

--- hello.py
+++ hello.py
@@ -1,5 +1,24 @@
-import math
+import math as m
 import sys
+import mymodule
+
 print("Hello World")
-print(math.pi)
+print(m.pi)
 print(sys.version)
+
+iseven = 55
+
+a = mymodule.iseven(3)
+print(a)
+print(mymodule.iseven(2))
+
+x = 5
+if x < 10:
+    print("toto")
+elif x < 20:
+    print("titi")
+else:
+    print("tutu")
+
+for i in range(18, 2, -2):
+    print(i)
---
+++ mymodule.py
@@ -0,0 +1,10 @@
+def iseven(x: int):
+    return x % 2 == 0
+
+# tp1
+# Créer un module monmodule
+# Créer la fonction isprime(nb:int) qui retourne True si nb est premier
+# Un nombre est premier si et seulement si il possède uniquement 2 diviseurs : 1 et nb
+# Tous les nombres >= 2 sont premiers sauf s'il possède un diviseur entre 2 et nb-1
+# Modulo : %
+# Tester 
