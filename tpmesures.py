c2c63d1f236c57a8e41c3a267c1dc9439ac769a8
cyrilvincent <contact@cyrilvincent.com>
2021-03-23 09:36:33 +0100

    CEA

--- tpmesures.py
+++ tpmesures.py
@@ -4,6 +4,7 @@
 
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
 with open("data/mesures/mesures.csv") as f:
     reader = csv.DictReader(f)
@@ -18,17 +19,45 @@ with open("data/mesures/mesures.csv") as f:
         vt.append(float(row["VT"]))
         am.append(float(row["AM"]))
         vm.append(float(row["VM"]))
-plt.subplot(221)
+    t = np.array(t)
+    at = np.array(at)
+    am = np.array(am)
+    vt = np.array(vt)
+    vm = np.array(vm)
+
+ferror = lambda x1, x2: np.abs(x1 - x2)
+ferror2 = lambda x1, x2: (x1 - x2) ** 2
+adif = ferror(at, am)
+# adif = np.abs(at - am)
+vdif = ferror(vt, vm)
+# vdif = np.abs(vt - vm)
+
+plt.subplot(321)
 plt.xlabel("Temps")
 plt.ylabel("AT")
 plt.plot(t, at)
-plt.subplot(222)
+plt.subplot(322)
 plt.plot(t, vt)
-plt.subplot(223)
+plt.subplot(323)
 plt.plot(t, am)
-plt.subplot(224)
+plt.subplot(324)
 plt.plot(t, vm)
+plt.subplot(325)
+plt.plot(t, adif)
+plt.subplot(326)
+plt.plot(t, vdif)
 plt.show()
+
+filter = np.abs(at - am) > 1
+afilter = am[filter]
+tfilter = t[filter]
+print(list(zip(tfilter, afilter)))
+
+filter = ferror(vt, vm) > 1
+vfilter = vm[filter]
+t2filter = t[filter]
+vtfilter = vt[filter]
+print(list(zip(t2filter, vfilter, vtfilter)))
 
 # TP Afficher les erreurs
 # ferreur = |courbe theorique - courbe mesurée| > bruit = 1v ou 1a
