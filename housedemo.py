f9df7f7214722d8c12413f2634fdbc80ab1e4cd1
cyrilvincent <contact@cyrilvincent.com>
2021-03-23 14:37:15 +0100

    CEA

--- housedemo.py
+++ housedemo.py
@@ -1,6 +1,7 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats
 
 def min_max_avg_std_median_quartile(a):
     return np.min(a), np.max(a), np.mean(a), np.std(a), np.median(a), np.quantile(a, 0.25),  np.max(a) - np.quantile(a, 0.75)
@@ -28,10 +29,14 @@ loyerparm2filter = loyerparm2[filter]
 loyer_par_m2_filter_stat = min_max_avg_std_median_quartile(loyerparm2filter)
 print(loyer_par_m2_filter_stat)
 
+slope, intercept, rvalue, pvalue, err = scipy.stats.linregress(surfaces, loyers)
+print(slope, intercept, rvalue, pvalue, err)
+
+faffine = lambda surface : slope * surface + intercept
 floyer = lambda surface : 37.4 * surface
 
 plt.scatter(surfaces, loyers)
-plt.plot(np.arange(150), floyer(np.arange(150)), color="red")
+plt.plot(np.arange(400), faffine(np.arange(400)), color="red")
 plt.show()
 
 # TP
