c5c600a29e798caf6d9c5e82556fd79e4bdccb55
cyrilvincent <contact@cyrilvincent.com>
2021-06-14 14:15:05 +0200

    CEA

--- demotuple.py
+++ demotuple.py
@@ -2,5 +2,8 @@ def min_max_avg(l):
     # Renvoyer en 1 seule itération le min, max, avg
     return 0,100,50
 
-min, max, avg = min_max_avg([])
+l = [1,2]
+len(l)
+min, max, avg = min_max_avg(2)
 print(min, max, avg)
+
