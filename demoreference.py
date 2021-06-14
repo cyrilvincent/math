80a11ed6f1f1653e197af27b6a4a60c90fe81643
cyrilvincent <contact@cyrilvincent.com>
2021-06-14 13:57:05 +0200

    CEA

--- demoreference.py
+++ demoreference.py
@@ -11,12 +11,14 @@ l2 = l1
 print(l1 == l2)
 l1.append(4)
 print(l1 == l2)
+print(l1 is l2) # True
 print(l1, l2)
 
 # Reference type with list()
 l1 = [1,2,3]
 l2 = list(l1)
-print(l1 == l2)
+print(l1 == l2) # True
+print(l1 is l2) # False
 l1.append(4)
 print(l1 == l2)
 print(l1, l2)
