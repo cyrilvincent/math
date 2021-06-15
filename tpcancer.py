3c130ba82b06404432703504606e1eeb292e1639
cyrilvincent <contact@cyrilvincent.com>
2021-06-15 16:09:06 +0200

    CEA

---
+++ tpcancer.py
@@ -0,0 +1,14 @@
+# Charger breast-cancer/data.csv avec pandas read_csv(...,index_col="id")
+# y = dataframe["diagnosis"] 0=benin 1 = malin
+# x = dataframe.drop("diagnosis",1).drop("id")
+
+# Sur x
+# Chercher 2 colonnes qui gère la taille et la concavité
+# Calculer sur ces 2 colonnes min,max,mean,std,median,quantile
+# Retenez la valeurs mean et std
+# Trouver une correlation
+# x_benin = 0
+# x_malin = 1
+
+# Bonus
+# Heart disease
