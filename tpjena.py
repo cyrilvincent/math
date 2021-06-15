8125532141a53fed107f8cec89edcec3c5792fdf
cyrilvincent <contact@cyrilvincent.com>
2021-06-15 11:34:41 +0200

    CEA

--- tpjena.py
+++ tpjena.py
@@ -11,7 +11,11 @@ bad_wv = wv == -9999.0
 wv[bad_wv] = 0.0
 dataframe.to_csv("data/jena/jena_filtered.csv", index=False)
 
-plt.plot(dataframe["Date Time"][:365*24:24], dataframe["T (degC)"][:365*24:24])
+N = 24*7
+conv = np.convolve(dataframe["T (degC)"], np.ones(N)/N)
+print(conv)
+plt.plot(dataframe["Date Time"][:365*5*24:24], dataframe["T (degC)"][:365*5*24:24])
+plt.plot(dataframe["Date Time"][:365*5*24:24], conv[:365*5*24:24])
 plt.show()
 
 # Afficher les T et les P dans des subplots
