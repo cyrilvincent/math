e866576fdd796bc4c1f3aa110478f564e62bec30
cyrilvincent <contact@cyrilvincent.com>
2021-06-16 10:29:12 +0200

    CEA

--- tpheart.py
+++ tpheart.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
 
 dataframe = pd.read_csv("data/heartdisease/data_cleaned_up.csv")
 y = dataframe.num
@@ -13,3 +15,11 @@ print(x[malin_filter].chol.describe())
 
 print(x[benin_filter].sex.describe())
 print(x[malin_filter].sex.describe())
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(x.age, x.chol)
+print(slope, intercept, r_value)
+f = lambda x: slope * x + intercept
+
+plt.scatter(x.age, x.chol)
+plt.plot(np.arange(28,66), f(np.arange(28,66)))
+plt.show()
