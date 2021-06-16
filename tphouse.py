6a77c10dba6d09e4b796c8b86e06d1aab1e78829
cyrilvincent <contact@cyrilvincent.com>
2021-06-16 10:20:06 +0200

    CEA

--- tphouse.py
+++ tphouse.py
@@ -6,15 +6,26 @@ import scipy.stats as stats
 # pip install scipy
 
 dataframe = pd.read_csv("data/house/house.csv")
+dataframe_filter = dataframe[dataframe.surface < 200]
 
-slope, intercept, r_value, p_value, std_err = stats.linregress(dataframe.surface, dataframe.loyer)
+avg = np.mean(dataframe_filter.loyer / dataframe_filter.surface)
+std = np.std(dataframe_filter.loyer / dataframe_filter.surface)
+print(avg, std)
 
+slope, intercept, r_value, p_value, std_err = stats.linregress(dataframe_filter.surface, dataframe_filter.loyer)
 print(slope, intercept, r_value, p_value, std_err)
 # f(x) = 41x - 283
+# f(x) = 34x + 122
 f = lambda x : slope * x + intercept
 
-plt.scatter(dataframe.surface, dataframe.loyer)
-plt.plot(np.arange(400), f(np.arange(400)), color='red')
+std_filter = np.abs(f(dataframe_filter.surface) - dataframe_filter.loyer) < 3 * std * dataframe_filter.surface
+dataframe_filter2 = dataframe_filter[std_filter]
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(dataframe_filter2.surface, dataframe_filter2.loyer)
+print(slope, intercept, r_value, p_value, std_err)
+
+plt.scatter(dataframe_filter2.surface, dataframe_filter2.loyer)
+plt.plot(np.arange(200), f(np.arange(200)), color='red')
 plt.show()
 
 # Interpréter le résultat
