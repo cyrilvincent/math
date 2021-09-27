89a1d5effe8247cf801fc9f557473619294b752e
cyrilvincent <contact@cyrilvincent.com>
2021-09-27 13:36:18 +0200

    CEA

---
+++ demo_tuple.py
@@ -0,0 +1,12 @@
+def return_tuple():
+    return 3,4
+
+a,b = return_tuple()
+print(a,b)
+
+# TP
+# Tester tp_liste filter map
+# Tester demo_value_ref
+# def min_max_avg(l):
+# Avec une boucle retourne min, max, avg
+# Tester
