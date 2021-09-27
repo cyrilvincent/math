d89e26eb7af2ba9567a7f67578182cde1872d3a0
cyrilvincent <contact@cyrilvincent.com>
2021-09-27 11:04:27 +0200

    CEA

--- premier.py
+++ premier.py
@@ -2,9 +2,16 @@ def is_even(x):
     return x % 2 == 0
 
 def is_prime(x):
-    return False
-    # x est premier sauf s'il possède un diviseur (x % qqchose == 0) entre 2 et x-1
+    if x < 2:
+        return False
+    else:
+        for denominateur in range(2,x):
+            if x % denominateur == 0:
+                return False
+    return True
+
 
 print(is_even(8))
 print(is_prime(7)) #True
 print(is_prime(8)) #False
+print(is_prime(4391))
