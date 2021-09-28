9373825970e812325e4e20ebecaacf13f5273459
cyrilvincent <contact@cyrilvincent.com>
2021-09-28 11:42:44 +0200

    CEA

--- demo_wav.py
+++ demo_wav.py
@@ -6,5 +6,6 @@ print(data.shape)
 if len(data.shape) == 2 and data.shape[1] == 2: #Because stereo
     data = data[:, 0]
 fft_out = np.fft.fft(data)
+print(fft_out.shape)
 plt.plot(np.abs(data), np.abs(np.real(fft_out)))
 plt.show()
