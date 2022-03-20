20424d767c1397b5b15989d00361dcf65992c15d
Cyril Vincent <contact@cyrilvincent.com>
2022-03-20 12:41:30 +0100

    CEA

--- demo_integrate.py
+++
@@ -1,5 +0,0 @@
-import scipy.integrate as integrate
-import numpy as np
-
-result = integrate.quad(np.sin, -np.inf, +np.inf)
-print(result)
--- demo_lambda.py
+++
@@ -1,7 +0,0 @@
-#f(x) = x + 1
-def f(x):
-    return x + 1
-# <=>
-f = lambda x : x + 1
-
-print(f(1))
