3200e772f0107d6a2ead68ff884125b0418c0b15
Cyril Vincent <contact@cyrilvincent.com>
2022-03-21 14:09:45 +0100

    CEA

--- tp_house.py
+++ tp_house.py
@@ -1,7 +1,11 @@
 import csv
 import matplotlib.pyplot as plt
+import mylib
 
 import numpy as np
+
+def f(x):
+    return x + 1
 
 surfaces = []
 loyers = []
@@ -20,14 +24,15 @@ print(f"min: {np.min(surfaces)}, max: {np.max(surfaces)}, avg: {np.mean(surfaces
 
 loyer_per_m2 = loyers / surfaces
 print(f"min: {np.min(loyer_per_m2)}, max: {np.max(loyer_per_m2)}, avg: {np.mean(loyer_per_m2)}")
+avg = np.mean(loyer_per_m2)
+y = surfaces * avg
 
+f = lambda x: avg * x
+g = lambda x: x ** 2
 
 # Afficher le loyer max, la surface max
 # Créer le tableau loyer/m², display, min, max, mean
 # Essayer d'afficher la fonction linéaire y = mean*x
 
-plt.subplot(211)
-plt.scatter(surfaces, loyers)
-plt.subplot(212)
-plt.scatter(surfaces, loyer_per_m2)
-plt.show()
+
+mylib.display(surfaces, loyers, loyer_per_m2, fn=lambda x: avg * x)
