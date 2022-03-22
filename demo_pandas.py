a60e8679a1ea060970cf65c669a1f6618663bc8d
Cyril Vincent <contact@cyrilvincent.com>
2022-03-22 16:01:13 +0100

    CEA

--- demo_pandas.py
+++ demo_pandas.py
@@ -1,18 +1,48 @@
 import pandas as pd
 import sqlite3
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import numpy as np
 
-#dataframe = pd.read_csv("data/house/house.csv")
+dataframe = pd.read_csv("data/house/house.csv")
 #dataframe = pd.read_excel("data/house/house.xlsx", index_col="id")
-with sqlite3.connect("data/house/house.db3") as conn:
-    dataframe = pd.read_sql("select * from house", conn)
+# with sqlite3.connect("data/house/house.db3") as conn:
+#     dataframe = pd.read_sql("select * from house", conn)
 print(dataframe)
 
-dataframe = dataframe[dataframe.loyer < 200]
+dataframe = dataframe[dataframe.surface < 200]
 
 loyers_serie = dataframe.loyer
 print(loyers_serie)
 
 print(dataframe.describe().T)
+
+slope, intercept, rvalue, pvalue, _ = stats.linregress(dataframe.surface, dataframe.loyer)
+print(slope, intercept)
+f1 = lambda x : slope * x + intercept
+
+avg = np.mean(dataframe.loyer / dataframe.surface)
+std = np.std(dataframe.loyer / dataframe.surface)
+
+
+
+
+plt.scatter(dataframe.surface, dataframe.loyer)
+plt.plot(np.arange(9, 200), f1(np.arange(9, 200)), color="green")
+plt.show()
+
+std_filter = np.abs(f1(dataframe.surface) - dataframe.loyer) < 3 * std * dataframe.surface
+dataframe_filter = dataframe[std_filter]
+slope2, intercept2, rvalue2, pvalue2, _ = stats.linregress(dataframe_filter.surface, dataframe_filter.loyer)
+print(slope2, intercept2)
+f2 = lambda x : slope2 * x + intercept2
+
+plt.scatter(dataframe_filter.surface, dataframe_filter.loyer)
+plt.plot(np.arange(9, 200), f2(np.arange(9, 200)), color="red")
+plt.show()
+
+
+
 
 # Afficher nuage de points surfaces / loyers
 # Calculer et afficher la regression lineaire
