894db4bd5d4f9af3a0780b90bec9bbe0c62735d2
Cyril Vincent <contact@cyrilvincent.com>
2022-03-22 13:41:37 +0100

    CEA

---
+++ tp_cancer.py
@@ -0,0 +1,5 @@
+# Charger data/breast-cancer/data.csv
+#la colonne index = id
+#ok_df, ko_df en fonction de diagnosis 1=malade
+#radius_worst, area_worst => describe
+# calculer np.mean, std, media, quartile
