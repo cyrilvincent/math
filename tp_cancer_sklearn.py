ccdbca26bde7830ec878395b50c47815fe99b997
Cyril Vincent <contact@cyrilvincent.com>
2022-03-23 13:59:20 +0100

    CEA

--- tp_cancer_sklearn.py
+++ tp_cancer_sklearn.py
@@ -6,3 +6,30 @@
 # Fit avec train
 # Score avec test
 # Biais
+
+import pandas as pd
+import sklearn.model_selection as ms
+import sklearn.neighbors as nn
+import numpy as np
+import sklearn.preprocessing as preproc
+import pickle
+
+np.random.seed(1)
+dataframe = pd.read_csv("data/breast-cancer/data.csv",index_col="id",na_values=".")
+y = dataframe.diagnosis
+x = dataframe.drop("diagnosis", axis=1)
+
+scaler = preproc.RobustScaler()
+scaler.fit(x)
+x = scaler.transform(x)
+
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x,y,train_size=0.8, test_size=0.2)
+
+model = nn.KNeighborsClassifier(3)
+model.fit(xtrain, ytrain)
+score = model.score(xtest, ytest)
+
+with open("data/breast-cancer/model.pickle", "wb") as f:
+    pickle.dump(model, f)
+
+
--- tp_heart_sklearn.py
+++ tp_heart_sklearn.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import sklearn.neighbors as nn
+import sklearn.ensemble as rf
+import matplotlib.pyplot as plt
 
 dataframe = pd.read_csv("data/heartdisease/data_with_nan.csv", na_values=".")
 dataframe = dataframe.drop(["slope", "ca"], axis=1).drop("thal", axis=1)
@@ -8,9 +10,17 @@ dataframe = dataframe.dropna()
 y = dataframe.num
 x = dataframe.drop("num", axis=1)
 
-model = nn.KNeighborsClassifier(3)
+# model = nn.KNeighborsClassifier(3)
+model = rf.RandomForestClassifier()
 model.fit(x, y)
 score = model.score(x, y)
 print(score)
+
+print(list(zip(x.columns, model.feature_importances_)))
+
+plt.bar(x.columns, model.feature_importances_)
+plt.show()
+
+
 
 
