89a0855e8d1e210f6e5834cbe8c39c8fd723040d
Cyril Vincent <contact@cyrilvincent.com>
2022-03-23 15:06:20 +0100

    CEA

--- tp_cancer_sklearn.py
+++ tp_cancer_sklearn.py
@@ -13,23 +13,34 @@ import sklearn.neighbors as nn
 import numpy as np
 import sklearn.preprocessing as preproc
 import pickle
+import sklearn.ensemble as rf
+import matplotlib.pyplot as plt
+import sklearn.neural_network as nn
 
 np.random.seed(1)
 dataframe = pd.read_csv("data/breast-cancer/data.csv",index_col="id",na_values=".")
 y = dataframe.diagnosis
-x = dataframe.drop("diagnosis", axis=1)
+xorig = dataframe.drop("diagnosis", axis=1)
 
 scaler = preproc.RobustScaler()
-scaler.fit(x)
-x = scaler.transform(x)
+scaler.fit(xorig)
+x = scaler.transform(xorig)
 
 xtrain, xtest, ytrain, ytest = ms.train_test_split(x,y,train_size=0.8, test_size=0.2)
 
-model = nn.KNeighborsClassifier(3)
+# model = nn.KNeighborsClassifier(3)
+# model = rf.RandomForestClassifier()
+model = nn.MLPClassifier((30,30,30))
 model.fit(xtrain, ytrain)
 score = model.score(xtest, ytest)
 
-with open("data/breast-cancer/model.pickle", "wb") as f:
+print(score)
+
+with open("data/breast-cancer/model_nn.pickle", "wb") as f:
     pickle.dump(model, f)
+
+# plt.bar(xorig.columns, model.feature_importances_)
+# plt.xticks(rotation=45)
+# plt.show()
 
 
