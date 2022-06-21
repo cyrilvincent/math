b01c60a9c616868ea4afce26581041672db9b023
Cyril Vincent <contact@cyrilvincent.com>
2022-06-21 14:10:30 +0200

    CEA

--- tp_heart.py
+++ tp_heart.py
@@ -2,4 +2,19 @@
 # Moy, Std sur chol, thalach et age
 # ko = dataframe[num == 1], ok = dataframe[num == 0]
 # Recalculer moy, std sur les 3 features
-# Ressayer la même chose en normalisant les data
+# Reessayer la même chose en normalisant les data
+
+import pandas as pd
+import numpy as np
+
+dataframe = pd.read_csv("data/heartdisease/dataclean.csv")
+print(dataframe.describe().T)
+
+ok = dataframe[dataframe.num == 0]
+ko = dataframe[dataframe["num"] == 1]
+
+print(ok.describe().T)
+print(ko.describe().T)
+
+chol_normalized = (dataframe.chol - np.mean(dataframe.chol)) / np.std(dataframe.chol)
+print(chol_normalized.describe())
