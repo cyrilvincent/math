22a68c721abe3f1fec69f2b02cc8a237f2f67b60
Cyril Vincent <contact@cyrilvincent.com>
2022-06-21 11:58:34 +0200

    CEA

---
+++ tp_heart.py
@@ -0,0 +1,5 @@
+# Charger data/heartdisease/dataclean.csv
+# Moy, Std sur chol, thalach et age
+# ko = dataframe[num == 1], ok = dataframe[num == 0]
+# Recalculer moy, std sur les 3 features
+# Ressayer la même chose en normalisant les data
