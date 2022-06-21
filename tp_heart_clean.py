f2006c4380ffb6046c314fa50e5be4c4834ff065
Cyril Vincent <contact@cyrilvincent.com>
2022-06-21 15:13:14 +0200

    CEA

--- tp_heart_clean.py
+++ tp_heart_clean.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
 dataframe = pd.read_csv("data/heartdisease/data_with_nan.csv", na_values=".")
-nbnan dataframe.isnull().sum()
+dataframe = dataframe.drop(["slope", "ca", "thal"], axis=1)
+dataframe = dataframe.dropna()
+# Supprimer les colonne slope,ca,thal
 # Effacer les lignes NaN
 # Bonus : Remplacer les null par la moyenne de la colonne
 # Sauvegarder le dataframe
 print(dataframe)
+dataframe.to_csv("data/heartdisease/dataclean.csv")
