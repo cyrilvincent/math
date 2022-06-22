8e14333d7c69115ed4e33bed35a71fb420705561
Cyril Vincent <contact@cyrilvincent.com>
2022-06-22 11:43:27 +0200

    CEA

--- demo_sklearn_heart.py
+++ demo_sklearn_heart.py
@@ -2,17 +2,37 @@ import sklearn
 import sklearn.linear_model as lm
 import sklearn.neighbors as nn
 import pandas as pd
+import sklearn.model_selection as ms
+import sklearn.preprocessing as pp
+import sklearn.pipeline as pipe
+import sklearn.preprocessing as preprocess
+import numpy as np
+
 
 dataframe = pd.read_csv("data/heartdisease/data_cleaned_up.csv")
 y = dataframe.num
 x = dataframe.drop("num", axis=1)
 
+
+
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, test_size=0.2, train_size=0.8)
+# JAMAIS de fit sur xtest
+
+scaler = preprocess.RobustScaler()
+scaler.fit(xtrain)
+xtrain = scaler.transform(xtrain) # lambda x: (x - mean) / std
+xtest = scaler.transform(xtest)
+
+np.median(xtrain)
+np.quantile([0.25, 0.75])
+
 # model = lm.LinearRegression()
-for k in range(1,12):
-    model = nn.KNeighborsClassifier(n_neighbors=k)
-    model.fit(x, y)
-    predicted = model.predict(x)
+model = nn.KNeighborsClassifier(n_neighbors=3)
+# model = pipe.make_pipeline(pp.PolynomialFeatures(5), lm.Ridge())
+model.fit(xtrain, ytrain)
+predicted = model.predict(x)
 # print(model.coef_, model.intercept_)
 
-    score = model.score(x, y)
-    print(f"Score: {score}")
+score_train = model.score(xtrain, ytrain)
+score_test = model.score(xtest, ytest)
+print(f"Score: {score_train} {score_test}")
