7595a19cab5fca1c678552ee35e142aa905710f9
Cyril Vincent <contact@cyrilvincent.com>
2022-09-19 08:16:29 +0200

    CEA

--- demo_fft.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-complex = 1j
-c2 = 3+2j
-print(complex ** 2)
-print(complex * c2)
--- hello.py
+++ hello.py
@@ -1,21 +1,6 @@
 import sys
 import numpy as np
 
-def inc(i:int) -> int:
-    return i+1
-
-
-inc(3)
 print("Hello World!")
 print(sys.version)
 print(np.__version__)
-i = 0
-while i < 10:
-    print(f"i={i}")
-    i = inc(i)
-
-a = 3 # Scalar
-l = [1,2,3] # List # Array # Vector # Serie
-m = [[1,2],[3,4]] # Array # Matrix #Dataframe
-# NDarray
-print(len(m))
