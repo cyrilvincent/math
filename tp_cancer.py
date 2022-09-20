7d524fb572b598ccb778ef03347277321b8e25a9
Cyril Vincent <contact@cyrilvincent.com>
2022-09-20 14:01:37 +0200

    CEA

---
+++ tp_cancer.py
@@ -0,0 +1,5 @@
+# Charger data/breast-cancer/data.csv
+# Virer la colonne id
+# dataframe_ok = diagnosis == 0
+# dataframe_ko = diagnosis == 1
+# Sur la colonne perimeter_worst calculer mean, std, medianne et quartile pour les 2 dataframes
