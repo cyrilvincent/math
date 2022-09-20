a2cfd7004476cf04d7ebe050792ebc9af52504f1
Cyril Vincent <contact@cyrilvincent.com>
2022-09-20 15:57:07 +0200

    CEA

--- demo_house.py
+++ demo_house.py
@@ -14,10 +14,30 @@ def f(x):
 
 x = np.arange(400)
 plt.plot(x, f(x), color="green")
-plt.show()
+
 
 print(f(20))
 
+dataframe = dataframe[dataframe.surface < 200]
+slope, intercept, r_value, p_value, std_err = stats.linregress(dataframe.surface, dataframe.loyer)
+print(slope, intercept, r_value, p_value, std_err)
+x = np.arange(200)
+plt.plot(x, f(x), color="red")
+plt.show()
+
+tarif_par_m2 = dataframe.loyer / dataframe.surface
+std = np.std(tarif_par_m2)
+print(std)
+
+dataframe_filtre = dataframe[np.abs(f(dataframe.surface) - dataframe.loyer) < 3 * std * dataframe.surface]
+plt.scatter(dataframe_filtre.surface, dataframe_filtre.loyer)
+slope, intercept, r_value, p_value, std_err = stats.linregress(dataframe_filtre.surface, dataframe_filtre.loyer)
+print(slope, intercept, r_value, p_value, std_err)
+x = np.arange(200)
+plt.plot(x, f(x), color="red")
+plt.show()
+plt.show()
+
 # Facile, refaire le tp en filtrant les points < 200
 # Calculer la std sur les tarifs/m²
-# Difficile, filtrer les appartement > 3 std et out recalculer
+# Difficile, filtrer les appartement > 3 std et tout recalculer
