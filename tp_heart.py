7f68e9adbe8cfa78e7508cf237980fce896cb2c3
Cyril Vincent <contact@cyrilvincent.com>
2022-09-20 11:46:40 +0200

    CEA

--- tp_heart.py
+++ tp_heart.py
@@ -15,3 +15,9 @@ dataframe_ko = dataframe[dataframe.num == 1]
 
 print(dataframe_ok.thalach.describe())
 print(dataframe_ko.thalach.describe())
+
+# Nettoyer le fichier en partant de dataframe
+# Virer les colonnes slope,ca,thal : dataframe.drop(["slope", "ca"], axis=1)
+# Virer les lignes avec des na
+# Vérifier le nombre de ligne supprimées (faire un describe avant et après suppression)
+# Sauvegarder dataframe dans data/heartdisease/dataclean.csv
