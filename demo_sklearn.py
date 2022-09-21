9d75fe84088c944c31424b0ce9218c6e7b4d9596
Cyril Vincent <contact@cyrilvincent.com>
2022-09-21 10:51:21 +0200

    CEA

--- demo_sklearn.py
+++ demo_sklearn.py
@@ -2,6 +2,9 @@ import sklearn.linear_model as lm
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import sklearn.preprocessing as pp
+import sklearn.pipeline as pipe
+import sklearn.linear_model as lm
 
 dataframe = pd.read_csv("data/house/house.csv")
 plt.scatter(dataframe.surface, dataframe.loyer)
@@ -9,7 +12,8 @@ plt.scatter(dataframe.surface, dataframe.loyer)
 x = dataframe.surface.values.reshape(-1, 1)
 y = dataframe.loyer
 
-model = lm.LinearRegression()
+# model = lm.LinearRegression()
+model = pipe.make_pipeline(pp.PolynomialFeatures(2), lm.Ridge())
 model.fit(x, y)
 
 predicted = model.predict(np.arange(400).reshape(-1, 1))
