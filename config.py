0e00affe6bf19ac4cdde1333e85e15e1bb0e0fe2
Cyril Vincent <contact@cyrilvincent.com>
2023-06-26 08:30:32 +0200

    CEA

--- config.py
+++
@@ -1,2 +0,0 @@
-house_csv_path = "data/house/house.csv"
-house_npz_path = "data/house/house.npz"
--- demo_matplotlib.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-plt.subplot(121)
-plt.bar(range(10), range(10))
-plt.subplot(122)
-plt.scatter(range(10), range(10))
-plt.savefig("myfig.png")
-plt.show()
