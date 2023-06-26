567afb3810d1086c59ffa73fad0b8499bbd6a9e9
Cyril Vincent <contact@cyrilvincent.com>
2023-06-26 16:34:27 +0200

    CEA

--- demo_house.py
+++ demo_house.py
@@ -1,14 +1,34 @@
 import csv
+import matplotlib.pyplot as plt
 
 with open("data/house/house.csv", "r") as f:
     reader = csv.DictReader(f)
+    surfaces = []
+    loyers = []
+    total = 0
     for row in reader:
         surface = float(row["surface"])
+        surfaces.append(surface)
         loyer = float(row["loyer"])
+        loyers.append(loyer)
         loyer_m2 = loyer / surface
+        total += loyer_m2
         print(surface, loyer, loyer_m2)
+    loyer_m2_avg = total / len(surfaces)
+
 
 # Afficher le nuage de point surface par loyer
 # Calculer le loyer / m2 moyen
 # Ecrire la lambda loyer_m2 * x
 # Afficher le résulat de la fonction affine loyer_m2 en superposition
+
+print(f"Loyer m2 moyen: {loyer_m2_avg}")
+
+f = lambda x: loyer_m2_avg * x
+
+f_result = [f(x) for x in range(400)]
+
+plt.scatter(surfaces, loyers)
+plt.plot(range(400), f_result, color="red")
+plt.show()
+
