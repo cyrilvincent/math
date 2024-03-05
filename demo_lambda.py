4b02f2b87fc7cd08bcfd6fe88491c76700117fab
Cyril Vincent <contact@cyrilvincent.com>
2024-03-05 14:48:05 +0100

    CEA

--- demo_lambda.py
+++
@@ -1,6 +0,0 @@
-def add(x, y):
-    return x + y
-
-add2 = lambda x,y : x + y
-
-print(add2(2,3))
--- demo_module.py
+++
@@ -1,9 +0,0 @@
-#import math
-# from math import sqrt
-import math
-
-def sqrt(x):
-    return "Toto"
-
-res = math.sqrt(4)
-print(res)
