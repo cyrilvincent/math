301480417fb00c7d87d8c9b92ca4eaa35f09d2ad
Cyril Vincent <contact@cyrilvincent.com>
2024-03-07 16:28:23 +0100

    CEA

--- demo_pandas.py
+++ demo_pandas.py
@@ -6,6 +6,9 @@ dataframe = pd.read_csv("data/house/house.csv")
 # dataframe = pd.read_excel("data/house/house.xlsx") # pip install openpyxl
 loyer = dataframe["loyer"]
 surface = dataframe.surface
+std = np.std(loyer)
+avg = np.mean(loyer)
+print(std)
 dataframe["loyer_per_m2"] = loyer / surface
 print(dataframe.describe())
 dataframe.to_json("data/house/house.json",indent=2)
@@ -14,6 +17,9 @@ dataframe = dataframe.drop("loyer_per_m2", axis=1)
 print(dataframe)
 
 dataframe = dataframe[dataframe.surface < 200]
+dataframe = dataframe[dataframe.loyer < avg + 3 * std]
+
+
 
 
 plt.scatter(dataframe.surface, dataframe.loyer)
