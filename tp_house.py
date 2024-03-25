d1c30a3992760d7f19466bf3a526ddcb62700671
cyrilvincent <contact@cyrilvincent.com>
2024-03-25 13:40:51 +0100

    CEA

--- tp_house.py
+++ tp_house.py
@@ -2,6 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 dict = np.load("data/house/house.npz")
+print(dict.keys())
 surface = dict["np_surfaces"]
 loyer = dict["np_loyers"]
 print(loyer)
+
+plt.scatter(surface, loyer)
+plt.show()
+
+# Afficher le max, min de surface et loyer
+# Créer le tableau loyer_per_m2
+# Afficher loyer_per_m2 dans un subplot
+# Filtrer les données pour ne garder que les surfaces < 200
+# Bonus : Mettre tout celà dans des fonctions avec de sparamètres réutilisables
+
