582b9f2e91e3df8f1340c569a740462e99976b8e
cyrilvincent <contact@cyrilvincent.com>
2024-03-26 09:42:13 +0100

    CEA

---
+++ tp_image.py
@@ -0,0 +1,28 @@
+import numpy as np
+from PIL import Image # pip install Pillow
+
+im = Image.open("data/foret.jpg")
+array = np.asarray(im).astype(np.float64)
+print(array.shape)
+
+array = array[::2,::2]
+array = array[:,:,0]
+
+
+dest = Image.fromarray(array.astype(np.uint8)).convert("RGB")
+dest.show()
+dest.save("data/modified.jpg")
+
+# TP
+# Tout faire dans des fonctions
+# load
+# save
+# show
+# transpose
+# get_channel(0 ou 1 ou 2)
+# crop(north, south, east, west)
+# reduct(n) enlever 1 colonne et une ligne sur n
+# change_lum(delta) np.clip(x,0,255)
+# change_lambda(l) : pour test 1.1*x-2
+# Bonus : add(im1, im2) : add 2 images (de même resolution)
+
