7a9e8ddd02de0ded0eb283e8f05505b97c68b7d2
cyrilvincent <contact@cyrilvincent.com>
2024-11-05 13:55:41 +0100

    CNRS

--- tp_image.py
+++ tp_image.py
@@ -4,3 +4,36 @@
 # Diviser la taille de l'image par 4 : prendre 1 ligne sur 2 et 1 colonne sur 2
 # Transpose
 # Bonus : tout mettre dans des fonctions get_chanel(0 ou 1 ou 2), crop(10,10,10,10)
+import numpy as np
+from PIL import Image
+
+def load(path: str) -> np.ndarray:
+    im = Image.open(path)
+    array = np.asarray(im).astype(np.float64)
+    return array
+
+def save(array: np.ndarray, path: str):
+    dest = Image.fromarray(array.astype(np.uint8)).convert("RGB")
+    dest.save(path)
+
+def get_chanel(array: np.ndarray, num: int) -> np.ndarray:
+    return array[:,:,num]
+
+def crop(array, north: int, south: int, east: int, west: int) -> np.ndarray:
+    return array[north:-south, east:-west]
+
+def divide(array, factor: int):
+    step = int(np.sqrt(factor))
+    return array[::step, ::step]
+
+def transpose(array):
+    return array.T
+
+if __name__ == '__main__':
+    array = load("data/ski.jpg")
+    print(array.shape)
+    array_nb = get_chanel(array, 1)
+    array_crop = crop(array_nb, 50, 100, 150, 200)
+    array_divide = divide(array_nb, 4)
+    array_transpose = transpose(array_divide)
+    save(array_transpose, "data/out.jpg")
