3ef0ff9b1dca5e32ff7a00a20a3ed77de6103d99
cyrilvincent <contact@cyrilvincent.com>
2024-11-06 10:01:51 +0100

    CNRS

--- tp_heart.py
+++ tp_heart.py
@@ -5,7 +5,22 @@ df = pd.read_csv("data/heartdisease/data_with_nan.csv", na_values=".")
 
 print(df.thal.isnull().sum())
 df = df.drop("thal", axis=1)
+df = df.drop("slope", axis=1)
+df = df.drop("ca", axis=1)
 print(df)
+print(df.isnull().sum())
+
+mean_chol = np.mean(df.chol)
+std_chol = np.std(df.chol)
+print(mean_chol, std_chol)
+
+df.fillna({"chol": mean_chol + (np.random.rand() - 0.5) * 2 * std_chol}, inplace=True)
+df = df.dropna()
+
+print(df.isnull().sum())
+
+df.to_csv("data/heartdisease/dataclean.csv")
+
 
 # Compter les NaN pour chaque colonne : df.chol.isnull().sum()
 # Si beaucoup => dropper la colonne
