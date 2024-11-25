4580bef17802fd0e882c89c9bdd4faff407715d1
Cyril Vincent <contact@cyrilvincent.com>
2024-11-25 08:23:04 +0100

    CEA

--- python_main.py
+++
@@ -1,8 +0,0 @@
-import tp_house_lib as house
-
-surfaces, loyers = house.load("data/house/house.npz")
-print(house.stats(surfaces))
-print(house.stats(loyers))
-print(house.compute_loyers_m2(loyers, surfaces))
-print(house.compute_loyers_m2(loyers=loyers, surfaces=surfaces))
-print(house.filter_by_surface(loyers, surfaces, 200))
