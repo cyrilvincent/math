0a0b46f47b70e82d53b111f6d3d9c62513e381df
cyrilvincent <contact@cyrilvincent.com>
2024-11-26 11:40:43 +0100

    CEA

--- tp_image.py
+++ tp_image.py
@@ -1,8 +1,9 @@
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
 
 def open(path):
-    im = Image.open("data/ski.jpg")
+    im = Image.open(path)
     array = np.asarray(im).astype(np.float64)
     return array
 
@@ -22,12 +23,36 @@ def crop(array, north, south, east, west):
 def reduce(array, factor):
     return array[::factor, ::factor]
 
+def luminance(array):
+    return np.mean(array)
+
+def contrast(array):
+    return np.std(array)
+
+def black_white(array):
+    return np.mean(array, axis=2)
+
+def vertical_profile(array):
+    array = black_white(array)
+    return np.mean(array, axis=1)
+
+
 
 if __name__ == '__main__':
     array = open("data/ski.jpg")
+    print(f"Luminance: {luminance(array):.2f}")
+    print(f"Contrast: {contrast(array):.2f}")
     red = get_chanel(array, 0)
     arrayt = transpose(array)
     cropped = crop(array,50,100, 150,200)
     reduced = reduce(array, 2)
-    save(reduced,"data/out.png")
+    bw = black_white(array)
+    save(bw,"data/out.png")
+
+    array2 = open("data/rond_blanc.jpg")
+    profiled = vertical_profile(array2)
+    plt.bar(np.arange(len(profiled)), profiled)
+    plt.show()
+
+
 
