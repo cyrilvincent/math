798f280a9d8d0c0aa969dc95adb313c47edbd684
cyrilvincent <contact@cyrilvincent.com>
2024-11-26 14:26:04 +0100

    CEA

--- tp_mesures.py
+++ tp_mesures.py
@@ -3,8 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 # pip install openpyxl
 
-df = pd.read_excel("data/mesures/mesures.xlsx", index_col="T")
-print(df)
+# df = pd.read_excel("data/mesures/mesures.xlsx", index_col="T")
+df = pd.read_csv("data/mesures/mesures.csv", index_col="T")
+df = df[df.index < 200]
+
+plt.subplot(211)
+plt.plot(df.VT)
+plt.plot(df.VM, color="red")
+
+
+diff = np.abs(df.VT - df.VM)
+plt.subplot(212)
+plt.plot(diff)
+plt.show()
+
+errors = df[np.abs(diff) > 0.01]
+print(errors)
 
 # Afficher VT et VM
 # diff = |VT - VM|
