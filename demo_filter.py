2f4f26dbbd934502ef014fbc9c436604250f37b3
Cyril Vincent <contact@cyrilvincent.com>
2025-02-17 08:27:29 +0100

    CEA

--- demo_filter.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-a = np.random.rand(100000000)
-a_filtered = a[a > 0.5]
-print(len(a_filtered))
-
--- house_main.py
+++
@@ -1,4 +0,0 @@
-import house_lib as h
-
-surfaces, loyers = h.load("data/house/house.npz")
-surfaces, loyers = h.filter(200, surfaces, loyers)
