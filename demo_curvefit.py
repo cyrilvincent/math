88ebd441daf700c7d1c09a99814caa429a157e74
cyrilvincent <contact@cyrilvincent.com>
2025-02-19 11:29:38 +0100

    CEA

--- demo_curvefit.py
+++ demo_curvefit.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
+import scipy.integrate as integrate
 
 np.random.seed(0)
 noise = 5
@@ -40,7 +41,11 @@ weightxsinx, convxsinx = opt.curve_fit(xsinx, x, y, bounds=[(0,0,1), (5, 1, 3)])
 print(weightxsinx)
 print(convxsinx)
 
+def diff(x):
+    return np.abs(xsinx(x, weightxsinx[0], weightxsinx[1], weightxsinx[2]) - poly4(x, weight4[0], weight4[1], weight4[2], weight4[3], weight4[4]))
 
+area, error = integrate.quad(diff, 0, 7)
+print(area, error)
 
 plt.scatter(x, y, label="data")
 plt.plot(x, poly2(x, weight2[0], weight2[1], weight2[2]), color="red", label="poly2")
@@ -48,4 +53,7 @@ plt.plot(x, poly3(x, weight3[0], weight3[1], weight3[2], weight3[3]), color="blu
 plt.plot(x, poly4(x, weight4[0], weight4[1], weight4[2], weight4[3], weight4[4]), color="green", label="poly4")
 plt.plot(x, xsinx(x, weightxsinx[0], weightxsinx[1], weightxsinx[2]), color="maroon", label="xsinx")
 plt.legend()
+plt.show()
+
+plt.plot(x, diff(x))
 plt.show()
