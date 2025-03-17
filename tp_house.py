6da078b38ea8e5ac28dc1c034616503f4bcc9f38
Cyril Vincent <contact@cyrilvincent.com>
2025-03-17 13:38:29 +0100

    Teledyne

--- tp_house.py
+++ tp_house.py
@@ -14,3 +14,19 @@ print(loyers.size, surfaces.size)
 # Pour chacun des 3 vecteurs, afficher min, max, sum/size
 # Afficher le nombre de surface > 200
 # Filtrer loyer ET surfaces par rapport à surface < 200
+
+loyer_per_m2 = loyers / surfaces
+print(loyer_per_m2)
+print(np.min(loyers), np.max(loyers), np.mean(loyers))
+print(np.min(surfaces), np.max(surfaces), np.mean(surfaces))
+print(np.min(loyer_per_m2), np.max(loyer_per_m2), np.mean(loyer_per_m2))
+
+surf200 = surfaces[surfaces > 200]
+nb = surf200.size
+print(nb)
+
+predicate = surfaces < 200
+surfaces_filtered = surfaces[predicate]
+loyers_filtered = loyers[predicate]
+
+print(surfaces_filtered)
