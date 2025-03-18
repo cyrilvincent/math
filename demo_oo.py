026f2aa56654a1fa3a58a88345649527a3d88756
Cyril Vincent <contact@cyrilvincent.com>
2025-03-18 16:23:32 +0100

    Teledyne

--- demo_oo.py
+++ demo_oo.py
@@ -2,8 +2,20 @@ class Mesure:
 
     def __init__(self, ip, batch, serie):
         self.ip = ip
-        self.batch = batch
+        self._batch = batch
         self.serie = serie
+
+    @property
+    def batch(self):
+        return self._batch
+
+    @batch.setter
+    def batch(self, value):
+        if value.startswith("B"):
+            self._batch = value
+        else:
+            raise ValueError("Bad Batch")
+
 
     def acquire(self):
         pass
@@ -13,3 +25,8 @@ class Mesure:
 
     def search(self, batch):
         pass
+
+if __name__ == '__main__':
+    mesure = Mesure("0.0.0.0", 123, 456)
+    mesure.batch = "C789"
+    print(mesure.batch)
