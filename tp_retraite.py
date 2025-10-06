1aece68a9a8dbc98e1504184e4a78564a7685868
Cyril Vincent <contact@cyrilvincent.com>
2025-10-06 11:47:35 +0200

    CNRS

---
+++ tp_retraite.py
@@ -0,0 +1,8 @@
+nb_trimestre = 20
+age = 25
+
+# < 62 ans taux_retraite = 0
+# > 67 ans taux_retraite = 1
+# Si né > 1968 il faut avoir 172 trimestre
+# Règle 62 ans + nb trimestre manquant => afficher l'age de retraite à taux plein
+
