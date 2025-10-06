7a46d0af36e9b5d9761ca534c29d1c6a516060e4
Cyril Vincent <contact@cyrilvincent.com>
2025-10-06 08:51:34 +0200

    CNRS

--- demo_skimage.py
+++
@@ -1,7 +0,0 @@
-import skimage as ski
-
-image = ski.data.coins()
-# ... or any other NumPy array!
-edges = ski.filters.sobel(image)
-ski.io.imshow(edges)
-ski.io.show()
--- main.py
+++
@@ -1,4 +0,0 @@
-import tp_pillow_oo
-
-service = tp_pillow_oo.ImageService()
-service.get_chanel()
