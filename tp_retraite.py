eb3550118b093ff17eed313f37cc813a2f357540
cyrilvincent <contact@cyrilvincent.com>
2025-10-06 11:57:18 +0200

    CNRS

--- tp_retraite.py
+++ tp_retraite.py
@@ -1,8 +1,22 @@
-nb_trimestre = 20
-age = 25
+nb_trimestre = 170
+age = 64
 
 # < 62 ans taux_retraite = 0
 # > 67 ans taux_retraite = 1
 # Si né > 1968 il faut avoir 172 trimestre
 # Règle 62 ans + nb trimestre manquant => afficher l'age de retraite à taux plein
 
+if age < 62:
+    print("Pas de retraite")
+elif age > 67:
+    print("Retraite à taux plein")
+else:
+    if nb_trimestre >= 172:
+        print("Retraite à taux plein")
+    else:
+        nb_trimestre_restant = 172 - nb_trimestre
+        age_prevu = age + nb_trimestre_restant / 4
+        if age_prevu >= 67:
+            print("Retraite à 67 ans")
+        else:
+            print(f"Retraite à {age_prevu} ans")
