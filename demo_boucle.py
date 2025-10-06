391aaa113147381da100d34ed4513a9c64e4f4d4
cyrilvincent <contact@cyrilvincent.com>
2025-10-06 14:07:07 +0200

    CNRS

--- demo_boucle.py
+++ demo_boucle.py
@@ -20,5 +20,32 @@ for i in range(11):
 # TP
 # Calculer la factorielle de n qui est une variable >= 1
 # Calculer la suite de fibonacci pour n > 1
-# f(n) = f(n-1) + (f(n-2) avec f(0)=0 et f(1)=1
+# f(n) = f(n-1) + f(n-2) avec f(0)=0 et f(1)=1
 # BONUS: n>1 dire si est est premier ou non
+
+n=5
+result = 1
+for i in range(2, n+1):
+    result *= i
+print(f"{n}!={result}")
+
+f0=0
+f1=1
+n=10
+for i in range(2, n+1):
+    result = f0 + f1
+    f0=f1
+    f1=result
+print(f"F({n})={result}")
+
+n=1223
+if n<=1:
+    print("Non premier")
+else:
+    for div in range(2, n):
+        if n % div == 0:
+            print("Non premier")
+            break
+    print("Premier")
+
+
