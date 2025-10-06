0aef1d811caab9fb11a0042e7df623441aeff6c2
Cyril Vincent <contact@cyrilvincent.com>
2025-10-06 11:11:40 +0200

    CNRS

--- hello.py
+++ hello.py
@@ -1,12 +1,14 @@
-print("Hello World")
-x = 1
-print("toto")
-print(x)
-y = input("Saisir la valeur de y: ")
-y = int(y)
-print(y + 1)
+# print("Hello World")
+# x = 1
+# print("toto")
+# print(x)
+# y = input("Saisir la valeur de y: ")
+# y = int(y)
+# print(y + 1)
 
 # tp
 year = 2025
-# Saisir votre année de naissance
-# Afficher votre age
+birth = int(input("Année de naissance: "))
+age = year - birth
+age = age
+print(f"Vous avez {age:03d} ans")
