71462a7be7d5fa2e78b60b577240f22387e52275
Cyril Vincent <contact@cyrilvincent.com>
2025-10-06 10:25:37 +0200

    CNRS

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("Hello World")
+x = 1
+print("toto")
+print(x)
+y = input("Saisir la valeur de y: ")
+y = int(y)
+print(y + 1)
+
+# tp
+year = 2025
+# Saisir votre année de naissance
+# Afficher votre age
