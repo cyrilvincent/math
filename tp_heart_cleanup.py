883eab7b273836b07918fb0bedef20968f16cd66
cyrilvincent <contact@cyrilvincent.com>
2025-10-08 11:47:25 +0200

    CNRS

--- tp_heart_cleanup.py
+++ tp_heart_cleanup.py
@@ -2,7 +2,27 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv("data/heartdisease/data_with_nan.csv", na_values=["", "."])
+# Je vire les colonnes avec trop de na
+ratio_slope = df["slope"].isnull().sum() / len(df)
+ratios = {}
+for col in df.columns:
+    ratios[col] = df[col].isnull().sum() / len(df)
+print(ratios)
+
+
+print(ratio_slope)
+df = df.drop(["slope", "ca", "thal"], axis=1)
+mean_chol = np.mean(df["chol"])
+std_chol = np.std(df["chol"])
+
+
+df = df.fillna({"chol": mean_chol + (np.random.rand() - 0.5) * std_chol})
+print(df.isnull().sum())
+df = df.dropna()
 print(df)
+df.to_csv("data/heartdisease/dataclean.csv")
+
+
 
 # 1 Compter les nan => isnull().sum()
 # 2 Les colonnes avec > 50% de nan: dropper les colonnes
