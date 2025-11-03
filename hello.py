e1f04aaa20c89e45470393e14e49fedc5e12bfdf
cyrilvincent <contact@cyrilvincent.com>
2025-11-03 11:09:35 +0100

    CEA

---
+++ hello.py
@@ -0,0 +1,13 @@
+import sys
+import numpy
+import numpy as np
+
+print(sys.version, numpy.__version__)
+np.cos()
+print("Hello World")
+x = 1
+y = x + 1
+x = x + 1
+x += 1
+x /= 2 # x = x / 2
+print(f"La valeur de x est: {x:.2f}")
