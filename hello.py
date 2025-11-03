fa09b888891bf38e98f2aca451561a2906e7bbe2
cyrilvincent <contact@cyrilvincent.com>
2025-11-03 11:31:38 +0100

    CEA

--- hello.py
+++ hello.py
@@ -3,7 +3,6 @@ import numpy
 import numpy as np
 
 print(sys.version, numpy.__version__)
-np.cos()
 print("Hello World")
 x = 1
 y = x + 1
@@ -11,3 +10,12 @@ x = x + 1
 x += 1
 x /= 2 # x = x / 2
 print(f"La valeur de x est: {x:.2f}")
+
+# x = input("Saisir x: ")
+# print(x / 2)
+
+n = 5
+result = 1
+for i in range(1, n+1):
+    result *= i
+print(result)
