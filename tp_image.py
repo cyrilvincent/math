64e08914e23879868d0a47dce5a6c7c4dbbf9985
cyrilvincent <contact@cyrilvincent.com>
2025-11-04 15:09:32 +0100

    CEA

--- tp_image.py
+++ tp_image.py
@@ -16,7 +16,7 @@ def get_chanel(array, num):
     return array[:,:,num]
 
 def show(array):
-    plt.imshow(array, cmap="Greys_r")
+    plt.imshow(array.astype(np.uint8), cmap="Greys_r")
     plt.show()
 
 def crop(array, north, south, east, west):
@@ -31,20 +31,52 @@ def flip(array):
 def superpose(a1, a2):
     return (a1 + a2) / 2
 
+def luminance(array):
+    return np.mean(array)
+
+def contrast(array):
+    return np.std(array)
+
+def black_and_white(array):
+    return np.mean(array, axis=2)
+
+def negative(array):
+    return 255 - array
+
+def row_profile(array):
+    return np.mean(array, axis=0)
+
+def col_profile(array):
+    return np.mean(array, axis=1)
+
+def norm(array):
+    return np.clip(((array - luminance(array)) / contrast(array)) * 255/4 + 255/2, 0, 255)
+
 
 if __name__ == '__main__':
-    array = load("data/mug.jpg")
+    array = load("data/ski.jpg")
+    n = norm(array)
+    show(n)
     red = get_chanel(array, 0)
     cropped = crop(red,100,50,200,50)
     reduced = reduce(red,2)
     flipped = flip(red)
     superposed = superpose(red, flipped)
-    save(superposed, "data/out.png")
-    show(superposed)
+    print(luminance(array), contrast(array))
+    nb = black_and_white(array)
+    neg = negative(nb)
+    array = load("data/rond_blanc.jpg")
+    rprofile = row_profile(array)
+    plt.subplot(211)
+    plt.plot(rprofile)
+    save(array, "data/out.png")
+    plt.subplot(212)
+    show(array)
 
 
-# Faire une fonction get_chanel(array, num) et qui me retourne la matrice du chanel, par exemple get_chanel(array, 0) -> red
-# Créer les fonctions load & save qui charge et sauvegarde l'image
-# Créer la fonction crop(array, north, south, east, west) crop(10,10,10,10) -> slice
-# Créer la fonction reduce(array, nb), par exemple reduce(array, 2) -> ca me divise la taille l'image par 4
-# Bonus créer la fonction superpose(array1, array2)
+# luminance
+# black_white
+# contrast : std
+# bonus : negative
+# horizontal_profile + vertical_profile
+# bonus : normalize : ((x - lum) / contrast) * 255/4 + 255/2 + np.clip(mat,0,255)
