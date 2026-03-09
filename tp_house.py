7b579900d4c7357cf9d45ce44aea59e2075899f6
cyrilvincent <contact@cyrilvincent.com>
2026-03-09 15:07:19 +0100

    CEA

--- tp_house.py
+++ tp_house.py
@@ -12,3 +12,22 @@ print(surfaces)
 # Afficher les surfaces > 200m²
 # Afficher les loyers dont la surface > 200m²
 # [(surface > 100) & (loyer < 1000)] & = AND, | OR, ! NOT tester surface > 100 ET loyer < 1000
+
+print(np.min(loyers), np.max(loyers), np.sum(loyers)/len(loyers))
+print(np.min(surfaces), np.max(surfaces), np.sum(surfaces)/len(surfaces))
+loyer_m2 = loyers / surfaces
+print(loyer_m2)
+print(surfaces[surfaces > 200])
+print(loyers[surfaces > 200])
+filter = surfaces > 200
+print(loyers[filter])
+filter2 = (surfaces > 100) & (loyers < 2500)
+print(loyers[filter2])
+print(surfaces[filter2])
+
+import pickle
+with open("data/house/house.pkl", "wb") as f:
+    pickle.dump(loyers, f)
+
+with open("data/house/house.pkl", "rb") as f:
+    loyers = pickle.load(f)
