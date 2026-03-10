204c2605ff7dbccae4e3f183da5564a1e9a738fc
cyrilvincent <contact@cyrilvincent.com>
2026-03-10 10:01:21 +0100

    CEA

---
+++ tp_mesures.py
@@ -0,0 +1,6 @@
+# Avec pandas lire data/mesures/mesures.csv
+# Mettre la colonne T en index avec la paramètre index_col
+# Afficher dans matplotlib les colonnes AM & VM
+# Ajouter la colonne PC (Puissance Calculée) = AM * VM
+# LA colonne VT correspond aux données théoriques = Créer la colonne VDiff = np.abs(VM - VT)
+# Afficher VT & VDiff
