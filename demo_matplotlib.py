7be6fb8106f9b09c900e8e7602f58f19c7dad842
cyrilvincent <contact@cyrilvincent.com>
2026-03-10 08:44:27 +0100

    CEA

---
+++ demo_matplotlib.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(-2*np.pi, 2*np.pi,0.1)
+y = np.sin(x)
+y2 = np.cos(x)
+
+plt.subplot(2,2,1)
+plt.title("sin x")
+plt.plot(x, y, label="sin")
+plt.plot(x, y2, color="red", label="cos")
+plt.legend()
+plt.subplot(2,2,4)
+plt.scatter(x, y, label="sin")
+plt.legend()
+plt.show()
---
+++ tp_matplotlib.py
@@ -0,0 +1,4 @@
+# Créer le tableau x de -5 à +5 avec un pas de 0.1
+# Afficher dans des subplots tanh la la sigmoide 1 / (1 + exp(-x))
+# Dans tp_house afficher un nuage de point surfaces / loyers en déduire un modèle "apparent"
+# Ne garder que les surfaces < 200m² et reafficher un nuage de points
