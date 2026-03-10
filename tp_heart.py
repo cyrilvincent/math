5494e1d83ff861351a24ff1b21a24f13992e29c6
cyrilvincent <contact@cyrilvincent.com>
2026-03-10 13:58:07 +0100

    CEA

--- tp_heart.py
+++ tp_heart.py
@@ -11,3 +11,29 @@
 # Faire un describe pour chacun des ok_df et ko_df
 # Qu'en déduire sur l'age, le chol, le genre, la talach et autres ...
 # Tester df.corr() et essayer de comprendre le résultat
+
+import pandas as pd
+import numpy as np
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", None)
+
+df = pd.read_csv("data/heartdisease/data_with_nan.csv", na_values=".")
+print(df.describe())
+print(df.isnull().sum())
+
+df = df.drop(["slope", "ca", "thal"], axis=1)
+mean_chol = df["chol"].mean()
+std_chol = df["chol"].std()
+df["chol"] = df["chol"].fillna(mean_chol + (np.random.rand() - 0.5) * 2 * std_chol)
+df = df.dropna()
+
+df.to_csv("data/heartdisease/dataclean.csv", index=False)
+
+ok_df = df[df["num"] == 0]
+ko_df = df[df["num"] == 1]
+
+print(ok_df.describe())
+print(ko_df.describe())
+
+print(df.corr()["num"])
