19a5f94ebec8f3e002d38da44285d4790ce7f738
cyrilvincent <contact@cyrilvincent.com>
2026-03-10 09:23:15 +0100

    CEA

--- tp_matplotlib.py
+++ tp_matplotlib.py
@@ -2,3 +2,32 @@
 # Afficher dans des subplots tanh la la sigmoide 1 / (1 + exp(-x))
 # Dans tp_house afficher un nuage de point surfaces / loyers en déduire un modèle "apparent"
 # Ne garder que les surfaces < 200m² et reafficher un nuage de points
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(-5,5,0.1)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+plt.figure(1)
+plt.subplot(2,1,1)
+plt.plot(x, np.tanh(x))
+plt.subplot(2,1,2)
+plt.plot(x, sigmoid(x))
+
+data = np.load("data/house/house.npz")
+surfaces = data["np_surfaces"]
+loyers = data["np_loyers"]
+
+filter = surfaces < 175
+surfaces175 = surfaces[filter]
+loyers175 = loyers[filter]
+
+plt.figure(2)
+plt.subplot(2,1,1)
+plt.scatter(surfaces, loyers)
+plt.subplot(2,1,2)
+plt.scatter(surfaces175, loyers175)
+plt.show()
