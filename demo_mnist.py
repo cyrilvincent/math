0b4fa0615847ec805e58625e98d67a54e30585d3
Cyril Vincent <contact@cyrilvincent.com>
2026-03-11 15:08:26 +0100

    CEA

---
+++ demo_mnist.py
@@ -0,0 +1,54 @@
+import numpy as np
+import sklearn.neighbors as nn
+import matplotlib.pyplot as plt
+import sklearn.ensemble as rf
+import sklearn.neural_network as neural
+import sklearn.metrics as metrics
+
+np.random.seed(42)
+
+with np.load("data/mnist/mnist.npz", allow_pickle=True) as f:
+    xtrain, ytrain = f["x_train"], f["y_train"]
+    xtest, ytest = f["x_test"], f["y_test"]
+
+print(xtrain.shape, ytrain.shape, xtest.shape, ytest.shape)
+xtrain = xtrain.reshape(-1, 28*28) # 764
+xtest = xtest.reshape(-1, 28*28)
+
+# model = nn.KNeighborsClassifier()
+model = rf.RandomForestClassifier()
+model.fit(xtrain, ytrain)
+ypredicted = model.predict(xtest)
+score = model.score(xtest, ytest)
+print(f"Score: {score:.3f}")
+
+print(metrics.classification_report(y_true=ytest, y_pred=ypredicted))
+print(metrics.confusion_matrix(y_true=ytest, y_pred=ypredicted))
+
+plt.matshow(model.feature_importances_.reshape(28, 28))
+plt.show()
+
+xtest = xtest.reshape(-1, 28, 28)
+select = np.random.randint(xtest.shape[0], size=12)
+
+for index, value in enumerate(select):
+    plt.subplot(3, 4, index + 1)
+    plt.axis("off")
+    plt.imshow(xtest[value], cmap=plt.cm.gray_r, interpolation="nearest")
+    plt.title(f"Predicted {ypredicted[value]}")
+plt.show()
+
+errors = ytest != ypredicted
+xerrors = xtest[errors]
+yerrors = ypredicted[errors]
+
+select = np.random.randint(xerrors.shape[0], size=12)
+
+for index, value in enumerate(select):
+    plt.subplot(3, 4, index + 1)
+    plt.axis("off")
+    plt.imshow(xerrors[value], cmap=plt.cm.gray_r, interpolation="nearest")
+    plt.title(f"Predicted {yerrors[value]}")
+plt.show()
+
+
