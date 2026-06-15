e6305ca0623e6373c286c76dffbb50c163855002
cyrilvincent <contact@cyrilvincent.com>
2026-06-15 15:06:05 +0200

    CEA

--- tp_house.py
+++ tp_house.py
@@ -16,5 +16,17 @@ print(loyers)
 # np.sum / .size
 # Créer le vecteur loyer_m2
 # Créer les vecteur surfaces_inf_25 qui concerne les surfaces < 25
-# Créer le veceur loyer_inf_25 dont la surface est < 25
+# Créer le vecteur loyer_inf_25 dont la surface est < 25
 # Afficher la moyenne des loyers < 25
+
+print(loyers.shape, surfaces.shape)
+print(loyers.min(), np.max(loyers), np.sum(loyers) / len(loyers))
+print(surfaces.min(), np.max(surfaces), surfaces.sum() / loyers.size)
+
+loyer_m2 = loyers / surfaces
+print(loyer_m2.min(), np.max(loyer_m2), loyer_m2.sum() / loyer_m2.size)
+
+filter = surfaces < 25
+surfaces_inf_25 = surfaces[filter]
+loyers_inf_25 = loyer_m2[filter]
+print(loyers_inf_25.min(), np.max(loyers_inf_25), loyers_inf_25.sum() / loyers_inf_25.size)
