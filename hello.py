89ad1ea7bc2f137c3768f8a21d41cc22758ac87a
cyrilvincent <contact@cyrilvincent.com>
2026-06-15 10:04:26 +0200

    CEA

---
+++ hello.py
@@ -0,0 +1,31 @@
+print("Hello World")
+
+x = 3
+y = 3.14
+
+if x < 5:
+    print("DDDD")
+else:
+    print("GGGG")
+
+if x == 3:
+    print("OK")
+
+for i in range(10):
+    print(i)
+
+def add(x, y):
+    return x + y
+
+print(add(2,3))
+
+print(5 % 2)
+
+x = x + 1
+x += 1
+
+y = 3
+if y == 4:
+    print("A")
+    print("B")
+print("C")
---
+++ tp1.py
@@ -0,0 +1,4 @@
+# Créer la fonction min(a, b) qui retourne le min de a et b
+# Créer la fonction is_even(i) qui retourne True si i est pair
+# Créer la fonction power(a, b) avec un for qui retourne a**b
+# Bonus : difficile is_prime(a) qui retourne True si a est premier, tout nb i >= 2 est premier sauf si possède un diviseur entre 2 et n-1
