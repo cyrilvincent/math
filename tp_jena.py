04b86bc18e07d238cbc1b3eb5851e01335c7b696
cyrilvincent <contact@cyrilvincent.com>
2026-06-16 09:24:07 +0200

    CEA

--- tp_jena.py
+++ tp_jena.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 data = np.load("data/jena/jena.npz")
 print(data)
@@ -8,5 +9,13 @@ x = np.arange(len(data))
 
 # Afficher les temps el preasures dans un subplot
 # Afficher une seul temperature par jour : donc prendre une temperature sur 24
+
+plt.subplot(2,1,1)
+plt.title("Temp")
+plt.plot(temps[11::24], color="red")
+plt.subplot(2,1,2)
+plt.title("Preasure")
+plt.plot(preasures[11::24])
+plt.show()
 
 
